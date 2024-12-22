8e7fc19823b4c58b58e304b7f5d8b0fb63cd67b9
Kilobena <kkoo.20002222@gmail.com>
2024-12-22 04:44:45 +0100

    token + refresh handled

--- src/service/Auth.js
+++ src/service/Auth.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import Cookies from "js-cookie"; // Importing js-cookie to manage cookies
+import Cookies from "js-cookie"; // Importing js-cookie to manage cookiesl
 import { jwtDecode } from 'jwt-decode';
 class Auth {
     constructor(baseURL) {
