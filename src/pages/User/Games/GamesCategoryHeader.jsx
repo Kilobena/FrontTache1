769a60a4ecae55ec4523414d415580bdd89e6029
devg0at <magrafay@outlook.com>
2024-12-23 20:22:27 +0500

    Update GamesCategoryHeader.jsx

--- src/pages/User/Games/GamesCategoryHeader.jsx
+++ src/pages/User/Games/GamesCategoryHeader.jsx
@@ -10,8 +10,7 @@ const GamesCategoryHeader = ({ openSearchModal }) => {
         <style jsx="true">{`
           .casino-header nav a.active img,
           .casino-header nav a:hover img {
-            filter: brightness(0) saturate(100%) invert(83%) sepia(25%)
-              saturate(2166%) hue-rotate(349deg) brightness(104%) contrast(90%);
+            filter: brightness(0) saturate(100%) invert(83%) sepia(25%) saturate(2166%) hue-rotate(349deg) brightness(104%) contrast(90%);
           }
           .casino-header nav a.active svg,
           .casino-header nav a:hover svg {
@@ -20,33 +19,21 @@ const GamesCategoryHeader = ({ openSearchModal }) => {
         `}</style>
         <div className="flex items-center">
           {/* Navigation Items */}
-          <nav className="flex items-center md:gap-5 md:px-4 whitespace-nowrap scrollbar-hide overflow-x-auto mr-auto">
+          <nav className="flex w-full items-center md:gap-5 md:px-4 whitespace-nowrap scrollbar-hide overflow-x-auto mr-auto">
             {GAMES_CATEGORY_NAV.map((item, index) => (
               <NavLink
                 key={index}
                 to={item.path}
                 className={({ isActive }) =>
-                  `flex flex-col min-w-[25%] md:min-w-max items-center justify-center gap-1 md:gap-2 px-2 py-2 md:px-2 md:py-2 md:rounded-lg text-xs md:text-sm font-semibold transition-all ${
-                    isActive
-                      ? "bg-[#1c1c1c] text-primary-yellow active"
-                      : "text-gray-300 hover:text-yellow-400 hover:bg-[#1c1c1c]"
+                  `flex flex-col min-w-[24%] md:min-w-max items-center justify-center gap-1 md:gap-2 md:px-2 py-2 md:rounded-lg text-xs md:text-sm font-semibold transition-all ${
+                    isActive ? "bg-[#1c1c1c] text-primary-yellow active" : "text-gray-300 hover:text-yellow-400 hover:bg-[#1c1c1c]"
                   }`
                 }
               >
-                <span
-                  role="img"
-                  aria-label={item.label}
-                  className="icon transition-transform"
-                >
-                  {item?.icon ? (
-                    <img
-                      src={item?.icon}
-                      alt={item.label}
-                      className="w-6 h-6"
-                    />
-                  ) : null}
+                <span role="img" aria-label={item.label} className="icon transition-transform">
+                  {item?.icon ? <img src={item?.icon} alt={item.label} className="w-6 h-6" /> : null}
                 </span>
-                <span className="text-center text-[15px]">{item.label}</span>
+                <span className="text-center lg:text-[15px] text-[10px]">{item.label}</span>
               </NavLink>
             ))}
           </nav>
@@ -59,19 +46,8 @@ const GamesCategoryHeader = ({ openSearchModal }) => {
                 className="cursor-pointer w-full text-left bg-white text-gray-800 pl-8 pr-3 py-2 rounded-[8px] shadow focus:outline-none focus:ring-2 focus:ring-yellow-500"
               />
               <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="#000"
-                  className="w-4 h-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="#000" className="w-4 h-4">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
               </span>
             </div>
