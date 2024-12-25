3eae35c2a43f63f44b328c62471e47d9fce2bd51
devg0at <magrafay@outlook.com>
2024-12-25 19:05:33 +0500

    Update SelectedTransactionDates.jsx

--- src/pages/Admin/FilterFormComponents/SelectedTransactionDates.jsx
+++ src/pages/Admin/FilterFormComponents/SelectedTransactionDates.jsx
@@ -2,21 +2,22 @@ import React, { useState } from "react";
 import { format } from "date-fns";
 import CustomDateRangePicker from "../../../components/ui/DateRangePicker";
 import { useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 const SelectedTransactionDates = ({ setShowTransferHistoryData, setSelectedDate, selectedDate, title }) => {
-  const [isDatePickerClose, setDatePickerClose] = useState(false);
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
   const [chosenDate, setChosenDate] = useState("");
   const [dateRangeState, setDateRangeState] = useState([
     {
       startDate: new Date(),
-      endDate: new Date(),
+      endDate: null,
       key: "selection",
     },
   ]);
   const location = useLocation();
   console.log(location.pathname, "Ssss");
   const handleCloseDate = () => {
-    setDatePickerClose(false);
+    setIsDatePickerOpen(false);
     setShowTransferHistoryData(false);
   };
 
@@ -45,7 +46,23 @@ const SelectedTransactionDates = ({ setShowTransferHistoryData, setSelectedDate,
               className={`flex md:flex-1 lg:w-full lg:min-w-[11rem] lg:last:min-w-[24rem] md:gap-3 items-center ml-0.5 p-2.5 cursor-pointer first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg rounded-lg sm:rounded-none   
                 ${selectedDate === option.value ? "bg-yellow-400 text-black shadow-md" : "bg-[#e2e2e2] text-gray-700"}  
                 ${location.pathname === "/agent/transfer-history" && index !== 4 ? "w-[48.9%] gap-1" : "w-full"}
-                                `}
+              `}
+              onClick={(e) => {
+                if (option.value === "custom") {
+                  e.preventDefault();
+                  setSelectedDate("custom");
+                  setIsDatePickerOpen(!isDatePickerOpen);
+                } else {
+                  setIsDatePickerOpen(false);
+                  setDateRangeState([
+                    {
+                      startDate: new Date(),
+                      endDate: null,
+                      key: "selection",
+                    },
+                  ]);
+                }
+              }}
             >
               <input
                 type="radio"
@@ -54,10 +71,6 @@ const SelectedTransactionDates = ({ setShowTransferHistoryData, setSelectedDate,
                 checked={selectedDate === option.value}
                 onChange={() => {
                   setSelectedDate(option.value);
-                  setDatePickerClose(true);
-                  if (option.value !== "custom") {
-                    setChosenDate("");
-                  }
                 }}
                 className="form-radio h-3 w-3"
               />
@@ -73,28 +86,32 @@ const SelectedTransactionDates = ({ setShowTransferHistoryData, setSelectedDate,
         </div>
       </div>
 
-      {selectedDate === "custom" && (
-        <div className="mb-8">
-          {isDatePickerClose && (
-            <>
-              <div className="flex flex-row justify-center gap-4">
-                <CustomDateRangePicker dateRangeState={dateRangeState} setDateRangeState={setDateRangeState} />
-              </div>
+      <AnimatePresence initial={false}>
+        {isDatePickerOpen && (
+          <div className="mb-8">
+            <motion.div initial={{ height: 0 }} animate={{ height: 1 }} exit={{ height: 0 }} key="slide" />
+            <div className="flex flex-row justify-center">
+              <CustomDateRangePicker dateRangeState={dateRangeState} setDateRangeState={setDateRangeState} />
+            </div>
 
-              <div className="flex justify-center mt-4 space-x-4">
-                <button
-                  className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-11  rounded-lg"
-                  disabled={!selectedDate}
-                  onClick={handleCloseDate}
-                >
-                  CLOSE
-                </button>
-                <button className="border border-black hover:bg-gray-300 text-gray-700 font-bold py-2 px-11 rounded-lg">CLEAR</button>
-              </div>
-            </>
-          )}
-        </div>
-      )}
+            <div className="flex justify-center mt-4 space-x-4">
+              <button
+                className="bg-primary-yellow hover:bg-[#ccaa00] text-black font-bold p-2 min-w-[150px] rounded-lg disabled:opacity-50"
+                disabled={!dateRangeState[0].endDate ? true : false}
+                onClick={handleCloseDate}
+              >
+                CLOSE
+              </button>
+              <button
+                className="border border-black hover:bg-gray-300 text-gray-700 font-bold p-2 min-w-[150px] rounded-lg disabled:opacity-50"
+                disabled={!dateRangeState[0].endDate ? true : false}
+              >
+                CLEAR
+              </button>
+            </div>
+          </div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
